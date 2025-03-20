686b2c7fdcd6e4feda3ed814d8e3ebbebf301f0e
Prateek Bhatt <prateekbhatt@Prateeks-MacBook-Air.local>
2025-03-20 22:40:12 +0530

    daily code

--- ValidParentheses.py
+++ ValidParentheses.py
@@ -1,16 +1,16 @@
 # Leetcode 20: valid parentheses
 def isValid(s: str) -> bool:
-        stack = []
-        bracket_map={')':'(','}':'{',']':'['}
-        for ch in s:
-            if ch in '{[(':
-                stack.append(ch)
-            else:
-                if not stack or stack[-1]!= bracket_map[ch]:
-                    # focus on use of or in condition check
-                    return False
-                stack.pop()
-        return not stack
+    stack = []
+    bracket_map={')':'(','}':'{',']':'['}
+    for ch in s:
+        if ch in '{[(':
+            stack.append(ch)
+        else:
+            if not stack or stack[-1]!= bracket_map[ch]:
+                # focus on use of or in condition check
+                return False
+            stack.pop()
+    return not stack
 
 print(isValid("()[]{}"))
 print(isValid("()[]{}}"))
